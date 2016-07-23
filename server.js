385c41398854120d109169193b243519e2dde780
Kelly <kelly_devlin@alumni.brown.edu>
2016-07-22 23:17:58 -0700

    fix spacing

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ server.use(restify.requestLogger());
 var io = socketio.listen(server.server);
 
 server.listen(8080, function () {
-  log.info('server listening at %s', server.url);
+    log.info('server listening at %s', server.url);
 });
 
 var game = new Game();
 
 // routes
 server.get('/getid/', function (req, res, next) {
-  req.log.info('Get id request initiated.');
-  if (!game.enoughPlayers) {
-    var id = generateId();
-    req.log.info({'playedId': id}, 'Id generated for player: ' + id + ', will redirect to /game/' + id + ' route');
-		res.redirect('http://localhost:8080/game/' + id, next);
-	} else {
-    req.log.info('Get id request with max players already reached, will redirect to root route');
-		res.redirect('http://localhost:8080/', next);
-	}
+    req.log.info('Get id request initiated.');
+    if (!game.enoughPlayers) {
+        var id = generateId();
+        req.log.info({'playedId': id}, 'Id generated for player: ' + id + ', will redirect to /game/' + id + ' route');
+        res.redirect('http://localhost:8080/game/' + id, next);
+    } else {
+        req.log.info('Get id request with max players already reached, will redirect to root route');
+        res.redirect('http://localhost:8080/', next);
+    }
 });
 
 server.get('game/:id', function indexHTML(req, res, next) {
@@ -46,76 +46,76 @@ server.get('game/:id', function indexHTML(req, res, next) {
 });
 
 server.get(/.*/, restify.serveStatic({
-	'directory': './client',
-	'default': 'welcome.html'
+    'directory': './client',
+    'default': 'welcome.html'
 }));
 
 // event listeners
 io.sockets.on('connect_error', function(err) {
-  log.error({err: err}, 'Socket connection error.');
+    log.error({err: err}, 'Socket connection error.');
 });
 
 io.sockets.on('connection', function (socket) {
-  log.info({socketId: socket.id}, 'Socket connected.');
-  socket.on('error', function (err) {
-    log.error({err: err, socketId: socket.id}, "Failed to connect to server.");
-  });
-	buildBoard(socket);
-	socket.on('column click', function(data) { handleColumnClick(socket, data); });
-	socket.on('new game', function () { handleNewGame(socket); });
+    log.info({socketId: socket.id}, 'Socket connected.');
+    socket.on('error', function (err) {
+        log.error({err: err, socketId: socket.id}, "Failed to connect to server.");
+    });
+    buildBoard(socket);
+    socket.on('column click', function(data) { handleColumnClick(socket, data); });
+    socket.on('new game', function () { handleNewGame(socket); });
 });
 
 var handleColumnClick = function(socket, data) {
-  log.info({socketId: socket.id}, 'Handling column click for colum: ' + data.column + ', by player: ' + data.playerId);
+    log.info({socketId: socket.id}, 'Handling column click for colum: ' + data.column + ', by player: ' + data.playerId);
 
-	if (!game.gameOver && (parseInt(data.playerId) === game.currentPlayer)) {
-    log.info({socketId: socket.id}, 'Confirmed game is not over and it is player\'s turn. Attempting to place tile.');
+    if (!game.gameOver && (parseInt(data.playerId) === game.currentPlayer)) {
+        log.info({socketId: socket.id}, 'Confirmed game is not over and it is player\'s turn. Attempting to place tile.');
 
-		var player = game.currentPlayer;
-		var column = parseInt(data.column);
-		var row = game.playTurn(column);
-		if (row >= 0) {
-      log.info({socketId: socket.id}, 'Clicked column is not full, placing tile for player ' + player + ' at row ' + row + ' and column ' + column);
-			io.sockets.emit('fill tile', {row: row, column: column, player: player});
+        var player = game.currentPlayer;
+        var column = parseInt(data.column);
+        var row = game.playTurn(column);
+        if (row >= 0) {
+            log.info({socketId: socket.id}, 'Clicked column is not full, placing tile for player ' + player + ' at row ' + row + ' and column ' + column);
+            io.sockets.emit('fill tile', {row: row, column: column, player: player});
 
-			if (game.isGameOver(row, column)) {
-        var winner = game.winner;
-        log.info({socketId: socket.id}, 'Game is over, winner: ' + winner);
-				io.sockets.emit('game over', { winner: winner });
-			} else {
-        log.info({socketId: socket.id}, 'Game is not over, end of player ' + player + '\'s turn.');
-				game.switchCurentPlayer();
-			}
-		}
-	}
+            if (game.isGameOver(row, column)) {
+                var winner = game.winner;
+                log.info({socketId: socket.id}, 'Game is over, winner: ' + winner);
+                io.sockets.emit('game over', { winner: winner });
+            } else {
+                log.info({socketId: socket.id}, 'Game is not over, end of player ' + player + '\'s turn.');
+                game.switchCurentPlayer();
+            }
+        }
+    }
 };
 
 var handleNewGame = function(socket) {
-  log.info({socketId: socket.id}, 'Handling new game request.');
-	game = new Game();
-	io.sockets.emit('go home');
+    log.info({socketId: socket.id}, 'Handling new game request.');
+    game = new Game();
+    io.sockets.emit('go home');
 };
 
 // helper methods
 var generateId = function() {
-	game.numPlayers++;
-	if (game.numPlayers === 2) { game.enoughPlayers = true; }
-	return game.numPlayers;
+    game.numPlayers++;
+    if (game.numPlayers === 2) { game.enoughPlayers = true; }
+    return game.numPlayers;
 };
 
 var buildBoard = function(socket) {
-  log.info({socketId: socket.id}, 'Building game board.');
-	if (game.gameOver) {
-    log.info({socketId: socket.id}, 'Game is over, winner: ' + game.winner);
-    socket.emit('game over', { winner: game.winner });
-  }
+    log.info({socketId: socket.id}, 'Building game board.');
+    if (game.gameOver) {
+        log.info({socketId: socket.id}, 'Game is over, winner: ' + game.winner);
+        socket.emit('game over', { winner: game.winner });
+    }
 
-	for(var row = 0; row < game.board.length; row++) {
-		for(var column = 0; column < game.board[row].length; column++){
-      var player = game.board[row][column];
-      if (player !== 0) { log.info({socketId: socket.id}, 'Placing tile for player ' + player + ' at row ' + row + ' and column ' + column); }
-			socket.emit('fill tile', {row: row, column: column, player: player});
-		}
-	}
-  log.info({socketId: socket.id}, 'Game board built.');
+    for(var row = 0; row < game.board.length; row++) {
+        for(var column = 0; column < game.board[row].length; column++){
+            var player = game.board[row][column];
+            if (player !== 0) { log.info({socketId: socket.id}, 'Placing tile for player ' + player + ' at row ' + row + ' and column ' + column); }
+            socket.emit('fill tile', {row: row, column: column, player: player});
+        }
+    }
+    log.info({socketId: socket.id}, 'Game board built.');
 };
--- test.js
+++ test.js
@@ -2,52 +2,52 @@ var Game = require('./game.js').Game;
 
 // mock game
 var generateCurrentGame = function(board, fillPosition, currentPlayer) {
-  var game = new Game();
-  game.board = board;
-  game.fillPosition = fillPosition;
-  game.currentPlayer = currentPlayer;
-  game.numPlayers = 2;
-  game.enoughPlayers = true;
-  return game;
+    var game = new Game();
+    game.board = board;
+    game.fillPosition = fillPosition;
+    game.currentPlayer = currentPlayer;
+    game.numPlayers = 2;
+    game.enoughPlayers = true;
+    return game;
 };
 
 // check equality
 var assertEquals = function(expected, actual, checkVariableName, testDescription) {
-  if (expected === actual) {
-    console.log("[success] verified " + checkVariableName + " with " + testDescription + ".");
-  } else {
-    console.log("[ERROR] failed to verify " + checkVariableName + " with " + testDescription + ".");
-  }
+    if (expected === actual) {
+        console.log("[success] verified " + checkVariableName + " with " + testDescription + ".");
+    } else {
+        console.log("[ERROR] failed to verify " + checkVariableName + " with " + testDescription + ".");
+    }
 };
 
 // test game result
 var checkGameResult = function(expectedsMap, game, rowPlayed, columnPlayed, testDescription) {
 
-  var actualsMap = {};
-  actualsMap.isGameOver = game.isGameOver(rowPlayed, columnPlayed);
-  actualsMap.hasWinner = game.hasWinner(rowPlayed,columnPlayed);
-  actualsMap.winner = game.winner;
-  actualsMap.checkNoMoreSpots = game.hasNoMoreSpots();
-  actualsMap.checkRow = game.checkRow(rowPlayed);
-  actualsMap.checkColumn = game.checkColumn(columnPlayed);
-  actualsMap.checkLeftDiagonal = game.checkLeftDiagonal(rowPlayed,columnPlayed);
-  actualsMap.checkRightDiagonal = game.checkRightDiagonal(rowPlayed,columnPlayed);
+    var actualsMap = {};
+    actualsMap.isGameOver = game.isGameOver(rowPlayed, columnPlayed);
+    actualsMap.hasWinner = game.hasWinner(rowPlayed,columnPlayed);
+    actualsMap.winner = game.winner;
+    actualsMap.checkNoMoreSpots = game.hasNoMoreSpots();
+    actualsMap.checkRow = game.checkRow(rowPlayed);
+    actualsMap.checkColumn = game.checkColumn(columnPlayed);
+    actualsMap.checkLeftDiagonal = game.checkLeftDiagonal(rowPlayed,columnPlayed);
+    actualsMap.checkRightDiagonal = game.checkRightDiagonal(rowPlayed,columnPlayed);
 
-  var expectsKeys = Object.keys(expectedsMap);
+    var expectsKeys = Object.keys(expectedsMap);
 
-  for (var i = 0; i < expectsKeys.length; i++) {
-    assertEquals(expectedsMap[expectsKeys[i]], actualsMap[expectsKeys[i]], expectsKeys[i], testDescription);
-  }
+    for (var i = 0; i < expectsKeys.length; i++) {
+        assertEquals(expectedsMap[expectsKeys[i]], actualsMap[expectsKeys[i]], expectsKeys[i], testDescription);
+    }
 };
 
 // test bottom row win
 var bottomRowWin = [
     [0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,2,2,2,0,0,0],
-		[1,1,1,1,2,0,0]
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,2,2,2,0,0,0],
+    [1,1,1,1,2,0,0]
 ];
 
 var game = generateCurrentGame(bottomRowWin, [4,3,3,3,4,4,4], 1);
@@ -67,11 +67,11 @@ checkGameResult(expected, game, 5, 0, 'bottom row win');
 // test top row win + full board
 var topRowWin = [
     [1,1,2,1,1,1,1],
-		[2,2,1,2,2,2,1],
-		[1,1,2,2,1,1,2],
-		[2,2,2,1,2,2,1],
-		[1,2,2,2,1,1,2],
-		[2,1,1,1,2,2,1]
+    [2,2,1,2,2,2,1],
+    [1,1,2,2,1,1,2],
+    [2,2,2,1,2,2,1],
+    [1,2,2,2,1,1,2],
+    [2,1,1,1,2,2,1]
 ];
 
 var game = generateCurrentGame(topRowWin, [-1,-1,-1,-1,-1,-1,-1], 1);
@@ -91,11 +91,11 @@ checkGameResult(expected, game, 0, 6, 'top row win');
 // test right column win
 var rightColumnWin = [
     [0,0,0,0,0,0,2],
-		[0,0,0,0,0,0,2],
-		[0,0,0,0,0,0,2],
-		[0,0,0,0,0,0,2],
-		[0,1,2,2,0,1,1],
-		[1,2,1,1,2,1,1]
+    [0,0,0,0,0,0,2],
+    [0,0,0,0,0,0,2],
+    [0,0,0,0,0,0,2],
+    [0,1,2,2,0,1,1],
+    [1,2,1,1,2,1,1]
 ];
 
 var game = generateCurrentGame(rightColumnWin, [4,3,3,3,4,3,-1], 2);
@@ -114,12 +114,12 @@ checkGameResult(expected, game, 0, 6, 'right column win');
 
 // test left column win full board
 var leftColumnFull = [
-  [2,1,2,1,2,1,2],
-  [2,1,1,2,2,2,1],
-  [2,2,1,2,1,1,2],
-  [2,1,1,2,2,1,1],
-  [1,1,2,1,2,1,2],
-  [1,2,1,2,1,2,1]
+    [2,1,2,1,2,1,2],
+    [2,1,1,2,2,2,1],
+    [2,2,1,2,1,1,2],
+    [2,1,1,2,2,1,1],
+    [1,1,2,1,2,1,2],
+    [1,2,1,2,1,2,1]
 ];
 
 var game = generateCurrentGame(leftColumnFull, [-1,-1,-1,-1,-1,-1,-1], 2);
@@ -139,11 +139,11 @@ checkGameResult(expected, game, 0, 0, 'left column board full');
 // test left diagonal win
 var leftDiagonalWin = [
     [0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,2],
-		[0,0,0,1,0,2,2],
-		[0,0,1,2,0,2,2],
-		[0,1,2,2,2,1,1],
-		[1,2,1,1,2,1,1]
+    [0,0,0,0,0,0,2],
+    [0,0,0,1,0,2,2],
+    [0,0,1,2,0,2,2],
+    [0,1,2,2,2,1,1],
+    [1,2,1,1,2,1,1]
 ];
 
 var game = generateCurrentGame(leftDiagonalWin, [4,3,2,1,3,1,0], 1);
@@ -162,12 +162,12 @@ checkGameResult(expected, game, 4, 1, 'left diagonal win');
 
 // test left diagonal win full board
 var leftDiagonalFull = [
-  [2,2,2,1,2,2,1],
-  [2,1,1,2,2,2,1],
-  [2,1,1,2,1,1,2],
-  [1,2,1,1,2,1,2],
-  [1,1,2,2,1,1,2],
-  [1,2,1,2,1,2,1]
+    [2,2,2,1,2,2,1],
+    [2,1,1,2,2,2,1],
+    [2,1,1,2,1,1,2],
+    [1,2,1,1,2,1,2],
+    [1,1,2,2,1,1,2],
+    [1,2,1,2,1,2,1]
 ];
 
 var game = generateCurrentGame(leftDiagonalFull, [-1,-1,-1,-1,-1,-1,-1], 1);
@@ -187,11 +187,11 @@ checkGameResult(expected, game, 0, 3, 'left diagonal win full board');
 // test right diagonal win
 var rightDiagonalWin = [
     [0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,2],
-		[0,0,0,1,0,0,2],
-		[0,0,0,2,1,2,2],
-		[0,1,2,2,2,1,1],
-		[1,2,1,1,2,1,1]
+    [0,0,0,0,0,0,2],
+    [0,0,0,1,0,0,2],
+    [0,0,0,2,1,2,2],
+    [0,1,2,2,2,1,1],
+    [1,2,1,1,2,1,1]
 ];
 
 var game = generateCurrentGame(rightDiagonalWin, [4,3,3,1,2,2,0], 1);
@@ -210,12 +210,12 @@ checkGameResult(expected, game, 2, 3, 'right diagonal win');
 
 // test right diagonal win full board
 var rightDiagonalFull = [
-  [1,1,2,2,2,1,2],
-  [1,2,1,2,2,2,1],
-  [2,1,2,2,1,2,2],
-  [2,1,2,1,2,1,2],
-  [1,1,2,1,1,1,2],
-  [1,2,1,2,1,2,1]
+    [1,1,2,2,2,1,2],
+    [1,2,1,2,2,2,1],
+    [2,1,2,2,1,2,2],
+    [2,1,2,1,2,1,2],
+    [1,1,2,1,1,1,2],
+    [1,2,1,2,1,2,1]
 ];
 
 var game = generateCurrentGame(rightDiagonalFull, [-1,-1,-1,-1,-1,-1,-1], 2);
@@ -234,12 +234,12 @@ checkGameResult(expected, game, 0, 3, 'right diagonal win full board');
 
 // test right diagonal win off board not full place final piece in middle
 var rightDiagonalFull = [
-  [1,0,0,0,0,1,0],
-  [1,0,1,2,0,2,0],
-  [2,0,1,2,2,1,0],
-  [2,1,2,1,2,2,2],
-  [1,1,2,1,1,1,2],
-  [1,2,1,2,1,2,1]
+    [1,0,0,0,0,1,0],
+    [1,0,1,2,0,2,0],
+    [2,0,1,2,2,1,0],
+    [2,1,2,1,2,2,2],
+    [1,1,2,1,1,1,2],
+    [1,2,1,2,1,2,1]
 ];
 
 var game = generateCurrentGame(rightDiagonalFull, [-1,2,0,0,1,-1,2], 2);
@@ -258,12 +258,12 @@ checkGameResult(expected, game, 2, 4, 'right diagonal win off board not full, pl
 
 // test full board no win
 var fullBoardNoWin = [
-  [1,1,2,2,2,1,2],
-  [1,2,1,2,2,2,1],
-  [2,1,2,2,1,1,2],
-  [2,1,2,1,2,1,2],
-  [1,1,2,1,1,1,2],
-  [1,2,1,2,1,2,1]
+    [1,1,2,2,2,1,2],
+    [1,2,1,2,2,2,1],
+    [2,1,2,2,1,1,2],
+    [2,1,2,1,2,1,2],
+    [1,1,2,1,1,1,2],
+    [1,2,1,2,1,2,1]
 ];
 
 var game = generateCurrentGame(fullBoardNoWin, [-1,-1,-1,-1,-1,-1,-1], 2);
